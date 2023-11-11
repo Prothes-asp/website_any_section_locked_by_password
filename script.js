fd80f2cefa1b10f3f621dd6f64f8c9b4b666e02e
Prothes Barai <75435992+Prothes-asp@users.noreply.github.com>
2023-11-12 01:27:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//  Section Password Code Start Here
+// =====================================
+// =====================================
+
+function onSubmit() {
+
+    var a = document.getElementById('password').value;
+    let myString = a;
+    let encodedValue = btoa(myString);
+
+    if (encodedValue == "NzQyNjcy" ) {
+        window.ProtectedDiv.style.display="block"; 
+
+        // When User Submitted Rignt Password
+        swal({
+            title: "Successed",
+            text: "Permission Granted",
+            icon: "success",
+            button: "Ok",
+          });
+    }
+
+    // When User Submitted Wrong Password
+    else if (encodedValue != "NzQyNjcy") {
+        swal({
+            title: "Wrong Password",
+            text: "Password Does Not Matched",
+            icon: "warning",
+            button: "Ok",
+        });
+    }
+
+}
+
+
+
+
+// When Filed is empty 
+
+$("#submit").click(function(){
+    var password = $("#password").val();
+    if(password == ''){
+        swal({
+            title: "Field is Empty ",
+            text: "Please Check The Field !",
+            icon: "warning",
+            button: "Ok",
+          });
+    }
+});
+
+
+
